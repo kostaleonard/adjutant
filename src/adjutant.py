cac96fd4cf7ce6db01861ad8c06641841ebd22d8
Leo Kosta <kostaleonard@gmail.com>
2021-10-06 12:25:59 -0400

    Added image upload
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- src/adjutant.py
+++ src/adjutant.py
@@ -1,4 +1,7 @@
+import logging
 import discord
+
+logging.basicConfig(level=logging.INFO)
 
 # TODO environment variable?
 with open('/Users/leo/.discord/token', 'r') as infile:
@@ -6,16 +9,23 @@ with open('/Users/leo/.discord/token', 'r') as infile:
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message(message):
+async def on_ready() -> None:
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message) -> None:
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('$pic'):
+        await message.channel.send(
+            'Here\'s that picture you wanted.',
+            file=discord.File('/tmp/my_image.png'))
 
 client.run(discord_token)
