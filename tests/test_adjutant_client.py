883998aa0fc6c57ce0b0a3717685bc82e9628519
Leo Kosta <kostaleonard@gmail.com>
2021-10-08 20:03:34 -0400

    Added experiment test
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- tests/test_adjutant_client.py
+++ tests/test_adjutant_client.py
@@ -1,6 +1,8 @@
 """Tests adjutant_client.py."""
 
+import os
 import time
+import json
 import pytest
 from discord.channel import TextChannel
 from adjutant import adjutant_client
@@ -11,10 +13,14 @@ WANDB_ENTITY = 'kostaleonard'
 DISCORD_TOKEN_ENVIRONMENT_VAR = 'DISCORD_ADJ_TOKEN'
 SETUP_TIMEOUT_SECONDS = 5
 NUM_KNOWN_PROJECT_RUNS = 60
+TEST_EXPERIMENT_SCRIPT = os.path.join('tests', 'write_arg.sh')
+TEST_EXPERIMENT_OUTPUT_FILE = os.path.join('/', 'tmp', 'adj_write_arg_out.txt')
 
 
 def test_adjutant_init_sets_public_fields() -> None:
     """Tests that Adjutant.__init__ sets the object's fields correctly."""
+    if not is_discord_config_present():
+        return
     adj = adjutant_client.Adjutant(WANDB_ENTITY, WANDB_PROJECT_TITLE)
     assert adj.channel_name
 
@@ -35,26 +41,48 @@ def test_adjutant_get_channel_finds_discord_channel() -> None:
 
 def test_adjutant_get_project_runs_finds_project_runs() -> None:
     """Tests that Adjutant._get_project_runs finds runs from known projects."""
-    if not is_wandb_config_present():
+    if not is_discord_config_present() or not is_wandb_config_present():
         return
     adj = adjutant_client.Adjutant(WANDB_ENTITY, WANDB_PROJECT_TITLE)
     runs = adj._get_project_runs()
     assert len(runs) >= NUM_KNOWN_PROJECT_RUNS
 
 
-def test_get_hyperparams_empty_str() -> None:
+def test_adjutant_get_hyperparams_empty_str() -> None:
     """Tests Adjutant._get_hyperparams on the empty string."""
     assert adjutant_client.Adjutant._get_hyperparams('') == {}
 
 
-def test_get_hyperparams_bad_json() -> None:
+def test_adjutant_get_hyperparams_bad_json() -> None:
     """Tests that Adjutant._get_hyperparams returns an empty dict when the input
     is improperly formatted."""
     assert adjutant_client.Adjutant._get_hyperparams('{"hello: 1}') == {}
 
 
-def test_get_hyperparams_valid_json() -> None:
+def test_adjutant_get_hyperparams_valid_json() -> None:
     """Tests that Adjutant._get_hyperparams returns the correct dict when the
     input represents valid JSON."""
     assert adjutant_client.Adjutant._get_hyperparams(
         '{"hello": 1, "world": "abc"}') == {"hello": 1, "world": "abc"}
+
+
+def test_adjutant_run_experiment_runs_command() -> None:
+    """Tests that Adjutant.run_experiment runs the specified command with the
+    hyperparameter JSON string as the argument."""
+    if not is_discord_config_present():
+        return
+    adj = adjutant_client.Adjutant(
+        WANDB_ENTITY,
+        WANDB_PROJECT_TITLE,
+        run_experiment_script=TEST_EXPERIMENT_SCRIPT)
+    hyperparams = {"hello": 1, "world": "abc"}
+    try:
+        os.remove(TEST_EXPERIMENT_OUTPUT_FILE)
+    except FileNotFoundError:
+        pass
+    adj.run_experiment(hyperparams)
+    assert os.path.exists(TEST_EXPERIMENT_OUTPUT_FILE)
+    with open(TEST_EXPERIMENT_OUTPUT_FILE, 'r') as infile:
+        contents = infile.read()
+    os.remove(TEST_EXPERIMENT_OUTPUT_FILE)
+    assert json.loads(contents) == hyperparams
---
+++ tests/write_arg.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "$1" > /tmp/adj_write_arg_out.txt
