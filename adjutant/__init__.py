e39f76abaa580d67460329280381b2372e5efae1
Leo Kosta <kostaleonard@gmail.com>
2021-10-15 07:22:45 -0400

    Better versioning
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- adjutant/__init__.py
+++ adjutant/__init__.py
@@ -1,8 +1,7 @@
 """adjutant is a package for managing ML experiments over Discord in conjunction
 with WandB."""
 
-from pkg_resources import get_distribution
 from adjutant import adjutant_client
 
-__version__ = get_distribution('adjutant-discord').version
+__version__ = '0.0.11'
 Adjutant = adjutant_client.Adjutant
--- setup.py
+++
@@ -1,5 +0,0 @@
-"""Packages the project for distribution on PyPI."""
-
-from setuptools import setup
-
-setup(version='0.0.10')
